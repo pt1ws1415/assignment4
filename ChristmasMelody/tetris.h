cd1d756f9a5b78772356826fb08952957248688e
Tim Neumann <tim.neumann@student.hpi.uni-potsdam.de>
2014-12-16 14:04:33 +0100

    Add tetris melody

---
+++ ChristmasMelody/tetris.h
@@ -0,0 +1,11 @@
+#ifndef TETRIS_H
+#define TETRIS_H
+#include "notes.h"
+
+#ifndef MELODY
+#  define MELODY {NOTE_E6, NOTE_B5, NOTE_C6, NOTE_D6, NOTE_C6, NOTE_B5, NOTE_A5, NOTE_A5, NOTE_C6, NOTE_E6, NOTE_D6, NOTE_C6, NOTE_B5, NOTE_C6, NOTE_D6, NOTE_E6, NOTE_C6, NOTE_A5, NOTE_A5, NOTE_D6, NOTE_F6, NOTE_A6, NOTE_G6, NOTE_F6, NOTE_E6, NOTE_C6, NOTE_E6, NOTE_D6, NOTE_C6, NOTE_B5, NOTE_B5, NOTE_C6, NOTE_D6, NOTE_E6, NOTE_C6, NOTE_A5, NOTE_A5, 0}
+#else
+#  error "Do not include more than one melody file!"
+#endif /* MELODY */
+
+#endif /* TETRIS */
